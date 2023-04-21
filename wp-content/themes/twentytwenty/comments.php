9795400cee961c4a620bcb2e2d545632bd61b5d9
nguyenHoangKinh <hoangkinh.tdc2223@gmail.com>
2023-04-21 22:50:07 +0700

    uplod fille

--- wp-content/themes/twentytwenty/comments.php
+++ wp-content/themes/twentytwenty/comments.php
@@ -126,7 +126,7 @@ if (comments_open() || pings_open()) {
 				<div class="col-2">
 
 				</div>
-				<div class="col-8">
+				<div class="col-8" style="height: 265px;">
 
 					<!--- Post Form Begins -->
 					<section class="card">
@@ -143,21 +143,8 @@ if (comments_open() || pings_open()) {
 								<div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
 									<div class="form-group">
 										<label class="sr-only" for="comment">Comment <span class="required">*</span></label>
-										<textarea class="form-control" id="comment" name="comment" cols="45" rows="3" maxlength="65525" required="" placeholder="What are you thinking..."></textarea>
+										<textarea class="form-control" style="height: 100px;" id="comment" name="comment" cols="45" rows="3" maxlength="65525" required="" placeholder="What are you thinking..."></textarea>
 									</div>
-									<div class="form-group">
-										<label class="sr-only" for="author">Name</label>
-										<input class="form-control" id="author" name="author" type="text" size="30" maxlength="245" autocomplete="name" required="" placeholder="Enter name">
-									</div>
-									<div class="form-group">
-										<label class="sr-only" for="email">Email</label>
-										<input class="form-control" id="email" name="email" type="email" size="30" maxlength="100" aria-describedby="email-notes" autocomplete="email" required="" placeholder="Enter email">
-									</div>
-									<div class="form-group">
-										<label class="sr-only" for="name">Website</label>
-										<input class="form-control" id="url" name="url" type="url" size="30" maxlength="200" autocomplete="url" placeholder="url of website">
-									</div>
-
 								</div>
 							</div>
 							<div class="text-right">
